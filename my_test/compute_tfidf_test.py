ad68b98f03cb747d812f9c1ca039b0ac33b16a70
blacksevenzqj <397353721@qq.com>
2020-01-28 01:41:58 +0800

    First

--- my_test/compute_tfidf_test.py
+++ my_test/compute_tfidf_test.py
@@ -14,3 +14,23 @@ def func(data):
 
 func(data)
 print(data)
+
+
+print("-" * 30)
+
+
+import json
+
+data = {
+    'name' : 'Connor',
+    'sex' : 'boy',
+    'age' : 26
+}
+print(data)
+data1=json.dumps(data)
+print(data1)
+data2=json.loads(data1)
+print(data2)
+print(type(data))#输出原始数据格式
+print(type(data1))#输出经过json.dumps的数据格式
+print(type(data2))#输出经过json.loads的数据格式
