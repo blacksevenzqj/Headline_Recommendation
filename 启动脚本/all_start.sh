eb9b04d157a92911faa4825c772589f25d05f7e0
blacksevenzqj <397353721@qq.com>
2020-01-31 02:20:52 +0800

    First

--- 启动脚本/all_start.sh
+++ 启动脚本/all_start.sh
@@ -5,3 +5,4 @@
 ./docker_start.sh
 ./hbase_start.sh
 ./hbase_thrift_start.sh
+./hive_metastore_start.sh
---
+++ 启动脚本/hive_metastore_start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "--------------HIVE_METASTORE_START---------------"
+hive --service metastore &
